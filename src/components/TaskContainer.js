720e18cc1340f735425c43a297d217c9d36e6ed6
DeepakMishraDA <deepakvinodmishra@gmail.com>
2021-11-24 00:06:36 +0100

    commit

--- src/components/TaskContainer.js
+++ src/components/TaskContainer.js
@@ -1,18 +1,11 @@
 import React from "react";
 import Button from "./Button";
 
-function TaskContainer({ task }) {
+function TaskContainer({ tasks }) {
   return (
     <div>
-      {task.map((item) => {
-        //console.log(item);
-        return (
-          <div>
-            <p>{item}</p>
-            <Button label="X" />
-          </div>
-        );
-      })}
+      <p>{tasks}</p>
+      <Button label="X" />
     </div>
   );
 }
