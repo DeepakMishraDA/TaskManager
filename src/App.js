0877137d02f32696c7bbe987b620b1e1f333755b
DeepakMishraDA <deepakvinodmishra@gmail.com>
2021-11-26 16:14:39 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -10,16 +10,18 @@ function App() {
 
   const todoHandler = (task) => {
     //the func will auto access event.target.value and then this set it into the copytodos array
+    //console.log("See", task);
     const copytodos = [...todos, task];
     setTodo(copytodos);
   };
   const todoRemover = (id) => {
     //the func will auto access event.target.value and then this set it into the copytodos array
-    //console.log("See", task);
-    console.log("Now See", id.target.value);
     const copytodos = [...todos];
     const filteredTodos = copytodos.filter((text) => {
-      return text !== id.target.value;
+      if (text === id) {
+        return false;
+      }
+      return true;
     });
     setTodo(filteredTodos);
   };
@@ -27,10 +29,10 @@ function App() {
   return (
     <div className="App">
       <HeadContainer updateTodo={todoHandler} todos={todos} />
-      {todos.map((tasks, index) => {
+      {todos.map((task, index) => {
         //console.log(index);
         return (
-          <TaskContainer tasks={tasks} id={tasks} removeTask={todoRemover} />
+          <TaskContainer tasks={task} id={task} removeTask={todoRemover} />
         );
       })}
     </div>
